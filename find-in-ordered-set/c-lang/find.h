22c1f21bc54501c1aac8cf63a03921bc1b1b5720
Josh Ramer <38678260+josh-ramer@users.noreply.github.com>
2019-12-07 08:44:22 -0600

    Create find.h

---
+++ find-in-ordered-set/c-lang/find.h
@@ -0,0 +1,13 @@
+#include <assert.h>
+#include <stdio.h>
+
+// Interview Cake C Unit Test Framework, based on CuTest
+#include "Cake/UnitTest.h"
+
+int contains(const int *a, size_t length, int n)
+{
+    // check if an integer is present in the vector
+    
+
+    return 0;
+}
