0f581bae2b342ca7be174989709dcdcad30b2a26
Josh Ramer <38678260+josh-ramer@users.noreply.github.com>
2019-12-07 08:30:02 -0600

    Create find.hpp

---
+++ find-in-ordered-set/cpp/find.hpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+// C++11 lest unit testing framework
+#include "lest.hpp"
+
+using namespace std;
+
+bool contains(const vector<int>& v, int n)
+{
+    // check if an integer is present in the vector
+    
+
+    return false;
+}
